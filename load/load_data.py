9e2d3ba9c4a10e17ba7c1862d9df4c1d6371820a
luis.mendez <a01688471@tec.mx>
2023-08-18 17:16:46 -0600

    Add .pkl file using Git LFS

--- load/load_data.py
+++ load/load_data.py
@@ -1,19 +1,19 @@
 import pandas as pd
-from typing import DataFrame
+#from typing import DataFrame
 
 #This function allows to load data set that will use
 
-def load_data(filepath: str) -> DataFrame:
-    """Load data from a CSV file.
-    
-    Args:
-        filepath (str): Path to the CSV file.
-    
-    Returns:
-        DataFrame: Loaded data.
-    """
-    data = pd.read_csv(filepath)
-    return data
+#def load_data(filepath: str) -> DataFrame:
+   # """Load data from a CSV file.
+   # 
+  #  Args:
+  #      filepath (str): Path to the CSV file.
+  #  
+  #  Returns:
+ #       DataFrame: Loaded data.
+ #   """
+ #   data = pd.read_csv(filepath)
+ #   return data
 
 
 
