cd7aa4fd0175f98ed10f173684e55e2167718850
luismeparra <140467342+luismeparra@users.noreply.github.com>
2023-08-18 16:31:17 -0600

    git add .

--- train/train_model.py
+++ train/train_model.py
@@ -2,20 +2,20 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from typing import Tuple
 
-def train_random_forest(X: DataFrame, y: Series) -> RandomForestClassifier:
-    """Train a RandomForestClassifier.
-    
-    Args:
-        X (DataFrame): Feature matrix.
-        y (Series): Target labels.
-    
-    Returns:
-        RandomForestClassifier: Trained model.
-    """
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    model = RandomForestClassifier(n_estimators=100, criterion='entropy', random_state=0, n_jobs=-1)
-    model.fit(X_train, y_train)
-    return model
+#def train_random_forest(X: DataFrame, y: Series) -> RandomForestClassifier:
+#    """Train a RandomForestClassifier.
+#    
+#    Args:
+#        X (DataFrame): Feature matrix.
+#        y (Series): Target labels.
+#    
+#    Returns:
+#        RandomForestClassifier: Trained model.
+#    """
+#    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+#    model = RandomForestClassifier(n_estimators=100, criterion='entropy', random_state=0, n_jobs=-1)
+#    model.fit(X_train, y_train)
+#    return model
 
 
 ######
