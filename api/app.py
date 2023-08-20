d650ce14a36b5924d82fa55bef355bd9370090ea
luis.mendez <a01688471@tec.mx>
2023-08-19 20:51:33 -0600

    git add .

--- api/app.py
+++ api/app.py
@@ -1,42 +1,45 @@
-from fastapi import FastAPI, HTTPException
-from train.train_pipeline import train_pipeline
-from preprocess.custom_transformers import CustomTransformer
+import os
+import sys
 import pandas as pd
+from fastapi import FastAPI, HTTPException
+from starlette.responses import JSONResponse
+#from predict.predict import ModelPredictor
+#from models.models import PassengerSatisfaction  # Update with your PassengerSatisfaction model
 import joblib
+
 app = FastAPI()
+
+# Add the parent directory to sys.path
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.abspath(os.path.join(current_dir, ".."))
+sys.path.append(parent_dir)
+
 # Load the trained model
 model_path = "./models/random_forest_model_output.pkl"
 loaded_model = joblib.load(model_path)
 
-# Define endpoint to train a new model
-@app.post("./train")
-def train_new_model():
-    # Load and preprocess data
-    data = pd.read_csv("./data/train.csv")
-    X = data.drop("satisfaction", axis=1)
-    y = data["satisfaction"]
+#predictor = ModelPredictor(model_path)
+#predictor = joblib.load(model_path)
 
-    # Train the model using your training function
-    trained_model = train_pipeline(X, y)
-
-    # Save the trained model
-    model_filename = "./models/new_trained_model.pkl"
-    joblib.dump(trained_model, model_filename)
-
-    return {"message": "New model trained and saved."}
-
-# Define endpoint to make predictions
-@app.post("./api/predict")
-def predict_new_value(data: dict):
+@app.get('/', status_code=200)
+async def healthcheck():
+    return 'Passenger satisfaction predictor is ready to go!'
+#1
+@app.post('/predict')
+def predict(passenger_features: dict) -> JSONResponse:
     try:
         # Convert input data to DataFrame and preprocess
-        input_df = pd.DataFrame(data, index=[0])
-        preprocessor = CustomTransformer(columns_to_encode=['Gender', 'Customer Type', 'Type of Travel', 'Class'])
-        input_df = preprocessor.transform(input_df)
+        input_data = {
+            "Gender": [passenger_features.gender],
+            "Age": [passenger_features.age],
+            "xx": [passenger_features.some_other_feature],  # Add other features here
+            # ...
+        }
+        input_df = pd.DataFrame(input_data)
 
         # Make prediction using the loaded model
-        prediction = loaded_model.predict(input_df)
+        prediction = predictor.predict(input_df)
 
-        return {"prediction": prediction.tolist()}
+        return JSONResponse({"prediction": prediction.tolist()})
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
