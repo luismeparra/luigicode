75213b99603ae58644e17b0cb5a7c7a54aaef418
luis.mendez <a01688471@tec.mx>
2023-08-19 18:27:18 -0600

    git add .

--- tests/tests.py
+++ tests/tests.py
@@ -1,25 +1,53 @@
+#!/usr/bin/env python
+
+"""Tests for your project package."""
 import os
 import pytest
 import pandas as pd
-from train.train_pipeline import train_pipeline  # Import your actual training function
+from preprocess.custom_transformers import CustomTransformer  # Import your custom transformer
 
-def test_train_pipeline():
-    # Sample data for testing
+def test_custom_transformer():
+    """
+    Test the `transform` method of your custom transformer.
+
+    This test checks if your custom transformer correctly encodes categorical variables
+    and returns the transformed DataFrame.
+
+    Make sure to replace 'CustomTransformer' with the actual name of your custom transformer class.
+    """
+    # Sample input data
     data = {
-        'Feature1': [1, 2, 3],
-        'Feature2': [4, 5, 6],
-        'satisfaction': [0, 1, 0]
+        'Gender': ['M', 'F', 'M'],
+        'Customer Type': ['Loyal', 'Regular', 'Loyal'],
+        'Type of Travel': ['Business', 'Leisure', 'Business'],
+        'Class': ['Business', 'Eco', 'Eco']
     }
     df = pd.DataFrame(data)
 
-    X = df.drop("satisfaction", axis=1)
-    y = df["satisfaction"]
+    # Instantiate your custom transformer
+    custom_transformer = CustomTransformer(columns_to_encode=['Gender', 'Customer Type', 'Type of Travel', 'Class'])
 
-    # Test train_pipeline function
-    model = train_pipeline(X, y)
+    # Transform the DataFrame using your custom transformer
+    df_transformed = custom_transformer.transform(df)
 
-    assert model is not None, "Training pipeline should return a model."
+    # Check if the transformed DataFrame has the expected shape
+    expected_shape = (3, 10)  # Update this with the actual shape after transformation
+    assert df_transformed.shape == expected_shape, \
+        f"The transformed DataFrame should have shape {expected_shape}"
+
+def test_csv_file_existence():
+    """
+    Test case to check if the CSV file exists.
+    """
+    # Provide the path to your CSV file that needs to be tested
+    csv_file_path = "./data/train.csv"  # Update this with the actual path
+
+    # Call the function to check if the CSV file exists
+    file_exists = os.path.isfile(csv_file_path)
+
+    # Use Pytest's assert statement to check if the file exists
+    assert file_exists == True, f"The CSV file at '{csv_file_path}' does not exist."
 
 if __name__ == "__main__":
-    # Run the test function using Pytest
+    # Run the test functions using Pytest
     pytest.main([__file__])
