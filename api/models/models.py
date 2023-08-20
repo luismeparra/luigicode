14b04958b83a89b6cff065d68971c3ed4fc7b409
luis.mendez <a01688471@tec.mx>
2023-08-20 10:01:12 -0600

    Your descriptive message

---
+++ api/models/models.py
@@ -0,0 +1,31 @@
+# models/models.py
+###
+from pydantic import BaseModel
+
+#####
+
+class PassengerSatisfaction(BaseModel):
+    id: int
+    Gender: str
+    Customer_Type: str
+    Age: float
+    Type_of_Travel: str
+    Class: str
+    Flight_Distance: float
+    Inflight_wifi_service: int
+    Departure_Arrival_time_convenient: int
+    Ease_of_Online_booking: int
+    Gate_location: int
+    Food_and_drink: int
+    Online_boarding: int
+    Seat_comfort: int
+    Inflight_entertainment: int
+    On_board_service: int
+    Leg_room_service: int
+    Baggage_handling: int
+    Checkin_service: int
+    Inflight_service: int
+    Cleanliness: int
+    Departure_Delay_in_Minutes: float
+    Arrival_Delay_in_Minutes: float
+    satisfaction: str
