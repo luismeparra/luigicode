578a785ddcb8e46dc49a0cf79c3bac4f6e981339
luis.mendez <a01688471@tec.mx>
2023-08-19 18:33:50 -0600

    git add .

--- tests/tests.py
+++ tests/tests.py
@@ -1,53 +1,46 @@
 #!/usr/bin/env python
 
-"""Tests for your project package."""
+"""Tests for `luigicode` package."""
 import os
 import pytest
 import pandas as pd
-from luigicode.preprocess.custom_transformers import CustomTransformer  # Import your custom transformer
 
-def test_custom_transformer():
+from sklearn.pipeline import Pipeline
+
+from luigicode.load.load_data import DataRetriever
+from luigicode.preprocess.preprocess_data import MissingIndicator
+
+def does_csv_file_exist(file_path):
     """
-    Test the `transform` method of your custom transformer.
+    Check if a CSV file exists at the specified path.
 
-    This test checks if your custom transformer correctly encodes categorical variables
-    and returns the transformed DataFrame.
+    Parameters:
+        file_path (str): The path to the CSV file.
 
-    Make sure to replace 'CustomTransformer' with the actual name of your custom transformer class.
+    Returns:
+        bool: True if the file exists, False otherwise.
     """
-    # Sample input data
-    data = {
-        'Gender': ['M', 'F', 'M'],
-        'Customer Type': ['Loyal', 'Regular', 'Loyal'],
-        'Type of Travel': ['Business', 'Leisure', 'Business'],
-        'Class': ['Business', 'Eco', 'Eco']
-    }
-    df = pd.DataFrame(data)
-
-    # Instantiate your custom transformer
-    custom_transformer = CustomTransformer(columns_to_encode=['Gender', 'Customer Type', 'Type of Travel', 'Class'])
-
-    # Transform the DataFrame using your custom transformer
-    df_transformed = custom_transformer.transform(df)
-
-    # Check if the transformed DataFrame has the expected shape
-    expected_shape = (3, 10)  # Update this with the actual shape after transformation
-    assert df_transformed.shape == expected_shape, \
-        f"The transformed DataFrame should have shape {expected_shape}"
+    return os.path.isfile(file_path)
 
 def test_csv_file_existence():
     """
     Test case to check if the CSV file exists.
     """
     # Provide the path to your CSV file that needs to be tested
-    csv_file_path = "./data/train.csv"  # Update this with the actual path
+    csv_file_path = "./data/retrieved_data.csv"  # Update with the actual path
+    
+    DATASETS_DIR = './data/'
+    
+    URL = 'https://www.openml.org/data/get_csv/16826755/phpMYEkMl'
+    data_retriever = DataRetriever(URL, DATASETS_DIR)
+    data_retriever.retrieve_data()
 
     # Call the function to check if the CSV file exists
-    file_exists = os.path.isfile(csv_file_path)
+    file_exists = does_csv_file_exist(csv_file_path)
 
     # Use Pytest's assert statement to check if the file exists
     assert file_exists == True, f"The CSV file at '{csv_file_path}' does not exist."
 
 if __name__ == "__main__":
-    # Run the test functions using Pytest
+    # Run the test function using Pytest
     pytest.main([__file__])
