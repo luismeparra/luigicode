a7cae149089ff3e8669fb2d51cd0a250ba0c6a2a
luis.mendez <a01688471@tec.mx>
2023-08-20 18:32:27 -0600

    git add .

--- tests/tests.py
+++ tests/tests.py
@@ -2,6 +2,9 @@
 
 import sys
 import os
+import logging
+
+
 
 # Add the parent directory to sys.path
 
@@ -24,6 +27,9 @@ from sklearn.pipeline import Pipeline
 #from load.load_data import DataRetriever
 from preprocess.preprocess_data import MissingIndicator
 
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+
 def does_csv_file_exist(file_path):
     """
     Check if a CSV file exists at the specified path.
@@ -34,6 +40,7 @@ def does_csv_file_exist(file_path):
     Returns:
         bool: True if the file exists, False otherwise.
     """
+    logging.info(f"Checking if the CSV file exists at '{file_path}'...")
     return os.path.isfile(file_path)
 
 def test_csv_file_existence():
@@ -50,12 +57,16 @@ def test_csv_file_existence():
     data_retriever = DataRetriever(URL, DATASETS_DIR)
     data_retriever.retrieve_data()
 
+    logging.info("Starting the test_csv_file_existence function...")
+    
     # Call the function to check if the CSV file exists
     file_exists = does_csv_file_exist(csv_file_path)
 
     # Use Pytest's assert statement to check if the file exists
     assert file_exists == True, f"The CSV file at '{csv_file_path}' does not exist."
 
+    logging.info("Finished the test_csv_file_existence function.")
+    
 if __name__ == "__main__":
     # Run the test function using Pytest
     pytest.main([__file__])
