d54d9ad5bf9ceaccd03724fdd6c6c573c053135a
luis.mendez <a01688471@tec.mx>
2023-08-20 18:07:39 -0600

    git add .

--- preprocess/custom_transformers.py
+++ preprocess/custom_transformers.py
@@ -1,12 +1,19 @@
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.preprocessing import LabelEncoder
 import pandas as pd
+import logging
 
 class CustomTransformer(BaseEstimator, TransformerMixin):
     """Custom transformer for data preprocessing."""
     
     def __init__(self, columns_to_encode):
         self.columns_to_encode = columns_to_encode
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.INFO)
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        file_handler = logging.FileHandler('transformations.log')
+        file_handler.setFormatter(formatter)
+        self.logger.addHandler(file_handler)
 
     def fit(self, X, y=None):
         return self
@@ -19,6 +26,7 @@ class CustomTransformer(BaseEstimator, TransformerMixin):
         label_encoder = LabelEncoder()
         for col in self.columns_to_encode:
             X_transformed[col] = label_encoder.fit_transform(X_transformed[col])
+            self.logger.info(f"Transformed column '{col}' using LabelEncoder")
 
         return X_transformed
 
