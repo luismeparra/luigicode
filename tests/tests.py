a6418878366fe45818f756a3a9c2b4b24326fb99
luis.mendez <a01688471@tec.mx>
2023-08-22 21:37:52 -0600

    git add .

--- tests/tests.py
+++ tests/tests.py
@@ -13,6 +13,7 @@ import logging
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir)))
 
 from load.load_data import DataRetriever
+from preprocess.preprocess_data import MissingIndicator
 
 
 
@@ -70,3 +71,73 @@ def test_csv_file_existence():
 if __name__ == "__main__":
     # Run the test function using Pytest
     pytest.main([__file__])
+
+
+def test_missing_indicator_transform():
+    """
+    Test the `transform` method of the MissingIndicator transformer.
+
+    This test checks if the transformer correctly adds indicator features for missing values
+    in the specified variables and returns the modified DataFrame.
+
+    The test case uses a sample DataFrame with missing values and a custom transformer instance.
+
+    It checks if the transformer successfully adds indicator features for the specified variables,
+    and the transformed DataFrame has the expected additional columns.
+
+    Note: Make sure to replace 'your_module' with the actual module name where the MissingIndicator class is defined.
+    """
+    # Sample DataFrame with missing values
+    data = {
+        'age': [25, 30, None, 40],
+        'income': [50000, None, 75000, 60000],
+        'gender': ['M', 'F', 'M', 'F']
+    }
+    df = pd.DataFrame(data)
+
+    # Instantiate the custom transformer with specified variables
+    missing_indicator = MissingIndicator(variables=['age', 'income'])
+
+    # Transform the DataFrame using the custom transformer
+    df_transformed = missing_indicator.transform(df)
+
+    # Check if the transformed DataFrame has the expected additional columns
+    expected_columns = ['age_nan', 'income_nan', 'gender']
+    assert all(col in df_transformed.columns for col in expected_columns), \
+        f"The transformed DataFrame should have the following additional columns: {expected_columns}"
+
+    expected_values = [0, 1, 0, 0]
+    assert all(df_transformed['income_nan'] == expected_values), \
+        f"Expected values for 'income_nan': {expected_values}"
+
+    # Check if the original DataFrame is not modified
+    assert 'age_nan' not in df.columns, "The original DataFrame should not be modified."
+
+def test_missing_indicator_fit():
+    """
+    Test the `fit` method of the MissingIndicator transformer.
+
+    This test checks if the `fit` method returns the transformer instance itself,
+    without performing any actual training or fitting.
+
+    The test case uses a sample DataFrame and a custom transformer instance.
+
+    Note: Make sure to replace 'your_module' with the actual module name where the MissingIndicator class is defined.
+    """
+    # Sample DataFrame
+    data = {
+        'age': [25, 30, 35, 40],
+        'income': [50000, 60000, 75000, 80000],
+        'gender': ['M', 'F', 'M', 'F']
+    }
+    df = pd.DataFrame(data)
+
+    # Instantiate the custom transformer without specifying variables
+    missing_indicator = MissingIndicator()
+
+    # Fit the transformer to the DataFrame
+    transformer_instance = missing_indicator.fit(df)
+
+    # Check if the fit method returns the transformer instance itself
+    assert transformer_instance == missing_indicator, \
+        "The `fit` method should return the transformer instance itself."
