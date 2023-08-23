8cad3edc2aee954c1cab8b0fa9ef18d383c1028c
luis.mendez <a01688471@tec.mx>
2023-08-23 17:07:08 -0600

    git add .

--- app/app.py
+++ app/app.py
@@ -1,9 +1,11 @@
 import sys
 import os
 
+# Add the parent directory to sys.path
 current_dir = os.path.dirname(os.path.abspath(__file__))
-parent_dir = os.path.abspath(os.path.join(current_dir, ".."))
+parent_dir = os.path.dirname(current_dir)
 sys.path.append(parent_dir)
+
 
 import logging
 
